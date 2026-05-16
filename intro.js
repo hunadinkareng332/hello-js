86f5f6bcd55ed833c4db1915eecae64e439e2976
Hunadi Nkareng <hunadinkareng@gmail.com>
2026-05-16 20:22:24 +0200

    Add intro.js file

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Hunadi";
+const age = 26;
+const favouriteLanguage = "JavaScript";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favourite Programming Language: " + favouriteLanguage);
