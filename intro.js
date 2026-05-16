e27a6240151ce0d4781480965d3abc3adb35a7ea
hunadinkareng332 <hunadinkareng@gmail.com>
2026-05-16 20:07:03 +0200

    Delete intro.js

--- intro.js
+++
@@ -1,7 +0,0 @@
-const name = "Hunadi";
-const age = 26;
-const favouriteLanguage = "JavaScript";
-
-console.log("Name: " + name);
-console.log("Age: " + age);
-console.log("Favourite Programming Language: " + favouriteLanguage);
